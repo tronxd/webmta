48740c93ee47393472a4bdc39617a1869c9927ad
AdiAzran <azran.adi@gmail.com>
2016-09-11 14:03:42 +0300

    Update MealProduct.js

--- models/MealProduct.js
+++ models/MealProduct.js
@@ -2,17 +2,17 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var MealProduct = new Schema({
-    _id   : String,
     date : Date,
     meal_number : Number,
     user_id : String,
     user_name : String,
     product_id : String,
     product_name : String,
+    unit_measure_caption: String,
+    unit_measure : String,
     calorie_per_Unit_measure : Number,
     amount : Number,
     total_calories : Number
-
 });
 
 module.exports = mongoose.model('meal_product', MealProduct);
