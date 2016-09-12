3caa3f52ae01631a61c0bd5f964eab5b7f4f4ade
AdiAzran <azran.adi@gmail.com>
2016-09-12 09:51:00 +0300

    Update MealProduct.js

--- models/MealProduct.js
+++ models/MealProduct.js
@@ -9,7 +9,7 @@ var MealProduct = new Schema({
     product_id : String,
     product_name : String,
     unit_measure_caption: String,
-    unit_measure : String,
+    unit_measure : Number,
     calorie_per_Unit_measure : Number,
     amount : Number,
     total_calories : Number
